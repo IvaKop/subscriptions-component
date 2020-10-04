4521f652c2a01ab9b8275daa1e62e4ca128a8142
Iva <ikopraleva@despark.com>
2020-10-04 20:16:18 +0300

    Updates

---
+++ precache-manifest.90d14bc324236d679bd6714515879515.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518b6c2d95dae539835657f324c839f2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3201f27e4f931b7cb926.bundle.js"
+  },
+  {
+    "url": "runtime~main.3201f27e4f931b7cb926.bundle.js"
+  },
+  {
+    "url": "vendors~main.3201f27e4f931b7cb926.bundle.js"
+  },
+  {
+    "url": "vendors~main.3201f27e4f931b7cb926.bundle.js.LICENSE.txt"
+  }
+]);
